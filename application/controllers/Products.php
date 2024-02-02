f47a750a1a9e07ee226e83d850ef912cabcde401
yemliha81 <yemlihaddln@hotmail.com>
2024-02-02 21:19:02 +0300

    Product insert

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -12,7 +12,81 @@ class Products extends CI_Controller {
 
     public function new_product()
 	{	
-		$this->load->view('new_product'); //views/new_product.php dosyasını göstermeye yarar.
+        $data['products'] = $this->db->select('*')
+            ->get('products')->result_array();
+
+
+		$this->load->view('new_product', $data); 
+	}
+
+    public function save()
+	{	
+        require $_SERVER['DOCUMENT_ROOT'] . '/simpleImage/SimpleImage.php';
+
+        //debug($_SERVER['DOCUMENT_ROOT']);
+
+
+		$post = $this->input->post();
+
+        $insert_array = [];
+
+        //Validasyon eklenebilir
+        $insert_array['product_name'] = $post['product_name'];
+        $insert_array['product_volume'] = $post['product_volume'];
+
+        foreach($_FILES as $key => $file){
+            $file = $_FILES[$key];
+            if( ( $file['type'] == 'image/jpeg' ) OR ( $file['type'] == 'image/png' ) ){
+                $img_name = img_seo_name( uniqid().'-'.$post['product_name']).'.jpg';
+                $from_file = $file['tmp_name'];
+                $to_file = $_SERVER['DOCUMENT_ROOT'] . '/files/' .$img_name;
+                $save_image = $this->save_image($from_file,$to_file, 300);
+                
+                if($save_image == true){
+                    $insert_array[$key] = $img_name;
+                }
+            }
+        }
+
+        //debug($insert_array);
+
+        $insert = $this->db->insert('products', $insert_array);
+
+        if($this->db->affected_rows() > 0){
+            echo "Kayıt başarılı!";
+        }else{
+            die("Error");
+        }
+
+        
+
+	}
+
+    private function save_image($from_file, $to_file, $width, $height=null){
+		try {
+		  // Create a new SimpleImage object
+		  $image = new \claviska\SimpleImage();
+
+		  // Magic! âœ¨
+		  $image
+			->fromFile($from_file)                     // load image.jpg
+			->autoOrient()                              // adjust orientation based on exif data
+			->resize($width, $height)                          // resize to 320x200 pixels
+			//->thumbnail($width, $height, 'center')        // resize to 320x200 pixels
+			//->flip('x')                                 // flip horizontally
+			//->colorize('DarkBlue')                      // tint dark blue
+			//->border('black', 10)                       // add a 10 pixel black border
+			//->overlay('watermark.png', 'bottom right')  // add a watermark image
+			->toFile($to_file, 'image/jpeg') ;     // convert to PNG and save a copy to new-image.png
+			//->toScreen();                               // output to the screen
+			return true;
+		  // And much more! ðŸ’ª
+		} catch(Exception $err) {
+		  // Handle errors
+		  echo $err->getMessage();
+		  return false;
+		  die();
+		}
 	}
 	
 }
--- application/views/new_product.php
+++ application/views/new_product.php
@@ -5,7 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Product</title>
 </head>
-<body>
+<body style="display:grid; grid-template-columns: 1fr 1fr 1fr;">
+    <div>
     Yeni Matrix Oluşturma
+        <form action="<?php echo PRODUCT_SAVE;?>" method="post" enctype="multipart/form-data">
+            <div>
+                <label for="">Resim 1</label>
+                <input type="file" name="photo_1">
+            </div>
+            <div>
+                <label for="">Resim 2</label>
+                <input type="file" name="photo_2">
+            </div>
+            <div>
+                <label for="">Ürün Adı</label>
+                <div>
+                    <input type="text" name="product_name">
+                </div>
+            </div>
+            <div>
+                <label for="">Ürün Hacim</label>
+                <div>
+                    <input type="number" name="product_volume">
+                </div>
+            </div>
+            <div>
+                <input type="submit" value="KAYDET">
+            </div>
+        </form>
+    </div>
+    <div>
+        Ürünler Matrisi
+        <div>
+            <?php foreach($products as $product) { ?>
+                <div>
+                    <?php echo $product['product_name'];?>
+                </div>
+                <div>
+                    <img src="<?php echo $_ENV['BASE_URL'];?>files/<?php echo $product['photo_1'];?>" width="100">
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 </body>
 </html>
